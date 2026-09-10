5d12ed1783ea8272c7bc9e199905f6be053aea07
X <112904595+Amirali-Ghofrani@users.noreply.github.com>
2026-09-10 01:49:16 -0700

    Preprocessing - Filtering th Data

---
+++ scripts/04_preprocessing.R
@@ -0,0 +1,43 @@
+### Practical code examples for data preprocessing:
+
+
+prj_dir <- dirname(getwd())
+data_dir <- file.path(prj_dir, "data")
+cel_files_dir <- file.path(data_dir, "raw_data")
+
+library(affy)
+
+my_cel_files <- ReadAffy(celfile.path = cel_files_dir)
+data <- exprs(my_cel_files)
+
+head(data)
+
+# General shape of our data
+hist(data, breaks = 500, xlim = c(0, 1000))
+hist(data, breaks = 5000, xlim = c(0, 500))
+
+### an example for filtering:
+# Filter genes based on their expression level
+data_filtered <- data[which(rowSums(data) < 100)]
+# Rows with a total gene expression less than 100
+
+data_filtered_2 <- data[which(rowMeans(data) > 5)]
+# Rows with a mean gene expression greater than 5
+
+
+
+# Using the sum() function
+sum(data == 0)
+# Number of zero values in the entire dataset
+
+sum(data <= 40)
+# Number of values equal to or less than 40 in the entire dataset
+
+
+
+
+
+
+
+
+
